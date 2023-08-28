f69ca037f1fe902fc0353dcd7f8341c06a88d647
AsadKhatri1 <92842688+AsadKhatri1@users.noreply.github.com>
2023-08-28 19:14:11 +0500

    commit main

---
+++ mind.js
@@ -0,0 +1,12 @@
+const linkedin=()=>{
+    window.open(
+        "https://www.linkedin.com/in/asad-khatri-22ba971ba/", "_blank");
+}
+const facebook=()=>{
+    window.open(
+        "https://www.facebook.com/asad.khatri.96930", "_blank");
+}
+const github=()=>{
+    window.open(
+        "https://github.com/AsadKhatri1", "_blank");
+}
